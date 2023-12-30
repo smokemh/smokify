bfa1fbe222022dd7c698de51000ec19bebde9307
smokemh <hasnatraja786@gmail.com>
2023-12-30 17:57:27 +0100

    remove typos in app.js

--- app.js
+++ app.js
@@ -1,47 +1,33 @@
 "use strict";
 
-
 // Node Modules
 
-const cors = require ("cors");
-const cookieParser = require ('cookie-parser');
-
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 // Initial express app
 const express = require("express");
 const app = express();
 
-
-
 // EJS setting
 
-app.set("view engine", 'ejs');
-
-
+app.set("view engine", "ejs");
 
 // Setting Static Directory
 
-app.use(express.static('${__dirname}/public'));
-
+app.use(express.static(`${__dirname}/public`));
 
 // Enable cors $ cookie parser
 app.use(cors()).use(cookieParser());
-
 
 // Custom Modules
 
 const login = require("./src/routes/login.route");
 
-
 // Login Page
 
-app.use('/login', login)
-
-
+app.use("/login", login);
 
 app.listen(5000, () => {
-    console.log("Server listening att http://localhost:5000");
-})
-
-
-
+  console.log("Server listening att http://localhost:5000");
+});
