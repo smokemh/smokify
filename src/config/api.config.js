7ba9797b6931081e68097fb1f7ed9e6255be700f
smokemh <hasnatraja786@gmail.com>
2024-01-14 19:11:26 +0100

    Configure Spotify API credentials and constants
    - Set up Spotify API client ID, client secret, redirect URI, and required scopes
    - Define constants for base URLs, authentication token state key, and API request parameters

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -2,3 +2,42 @@
 
 // Node Modules
 require("dotenv").config();
+
+// The Base Address of web api
+const BASE_URL = "https://api.spotify.com/v1";
+
+// The base Address of spotify authentication token
+const TOKEN_BASE_URL = "https://accounts.spotify.com/api";
+
+// Spotify Client ID
+const CLIENT_ID = process.env.CLIENT_ID;
+
+// Spotify Client Secret
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+// Spotify Redirect URI
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+// Spotify Scopes
+const SCOPES = process.env.SCOPES;
+
+// Authentication Token State key
+const STATE_KEY = "spotify_auth_state";
+
+// API request query params
+const MARKET = "US";
+const LOW_LIMIT = 12;
+const DEFAULT_LIMIT = 28;
+
+module.exports = {
+  BASE_URL,
+  TOKEN_BASE_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI,
+  SCOPES,
+  STATE_KEY,
+  MARKET,
+  LOW_LIMIT,
+  DEFAULT_LIMIT,
+};
